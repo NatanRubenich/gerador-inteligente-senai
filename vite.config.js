8293fd04ba9d8954cfba705d1c1286a64149547e
Natan Rubenich <natan.rubenich@unemat.br>
2025-12-13 03:02:48 -0300

    Fix base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/gerador-provas-senai/',
+  base: '/gerador-inteligente-senai/',
   build: {
     outDir: 'dist'
   }
